b4f61e58eafa16179e4f90814a5ec0fac2cd26a1
julienmalard <julien.malard@mail.mcgill.ca>
2025-05-14 17:38:44 +0530

    உண்மையான மாறிலிகள்

--- மூலம்/மாறிலிகள்.ts
+++ மூலம்/மாறிலிகள்.ts
@@ -1,67 +1,39 @@
 import type { bds } from "@constl/ipa";
-import { தேதி_மாறி_அடையாளம், தேதி_நெடுவரிசை_அடையாளம் } from "@lassi-js/kili";
+import { தேதி_நெடுவரிசை_அடையாளம் } from "@lassi-js/kili";
 
-export const மொழிபெயர்ப்பு_அட்டவணை_சாபி = "மொழிபெயர்ப்புகள்";
+export const மொழிபெயர்ப்பு_அட்டவணை_சாபி = "c12bdc50-4ba5-4103-85b6-47083cab1a43";
 
-export const சாபி_நெடுவரிசை_அடையாளம் = "சாபி";
-export const மூல்_மொழி_நெடுவரிசை_அடையாளம் = "மூல்_மொழி";
-export const இலக்கு_மொழி_நெடுவரிசை_அடையாளம் = "இலக்கு_மொழி";
-export const மூல்_உரை_நெடுவரிசை_அடையாளம் = "மூல்_உரை";
-export const மொழிபெயர்ப்பு_நெடுவரிசை_அடையாளம் = "மொழிபெயர்ப்பு";
+export const சாபி_நெடுவரிசை_அடையாளம் = "c6da1948-53f6-43ab-aea4-c16c1e0827d5";
+export const மூல்_மொழி_நெடுவரிசை_அடையாளம் = "fab36dd3-3b29-4672-ac1e-8908090022b3";
+export const இலக்கு_மொழி_நெடுவரிசை_அடையாளம் = "189becec-67b0-4da5-9357-fb3c68b2ee8c";
+export const மூல்_உரை_நெடுவரிசை_அடையாளம் = "0f337a12-9177-49c4-a6b2-3e75bc18faed";
+export const மொழிபெயர்ப்பு_நெடுவரிசை_அடையாளம் = "5a2f0b24-eca8-49a8-9b03-5475ea3b2e76";
 
 export const கிளிமூக்கு_மூல்_கூட்ட_அடையாளம் =
-  "/orbitdb/zdpuAyiNBJZCMSTWynQrRynKCbfz7uJhv892B5Dy1qnyQfwP6";
-
-export const சாபி_மாறி_அடையாளம் =
-  "/orbitdb/zdpuAviHxRLaTdUCfJeh1Dw8hbiHkcRsmEkiitX2MXQes3XYY";
-export const மூல்_மொழி_மாறி_அடையாளம் =
-  "/orbitdb/zdpuAyCPoNvoFVRt4y5uA97d8LLjcXnqs2CaoS6oVvbZwkSou";
-export const இலக்கு_மொழி_மாறி_அடையாளம் =
-  "/orbitdb/zdpuB35kfHohXZQnFFAMhwFPt78vH7e1NuquQ6KcNMPGQ3Hgj";
-export const மூல்_உரை_மாறி_அடையாளம் =
-  "/orbitdb/zdpuAwxQnCrzYK3kXtM4MGYfdWhM3rjSb1cyQx4hMSwjHzgf9";
-export const மொழிபெயர்ப்பு_மாறி_அடையாளம் =
-  "/orbitdb/zdpuB3HFW5njgmT9kxvJoxNq7wG27iCj8GLf3F3iKtfecw8De";
-
-export const மொழியாக்கம்_சிறப்பு_சொல்_அடையாளம் =
-  "/orbitdb/zdpuAnASCNyc198pcsxDYCdZoAvfj5EC3iWbeMyK9FRsAaoaS";
-export const கிளிமூக்கு_சிறப்பு_சொல்_அடையாளம் =
-  "/orbitdb/zdpuAzksPz4pdkBfy1Z74LStFLGPS3njxtD6BEvcPEYdSEv8R";
+  "/orbitdb/zdpuAn94dUjyHmjVbXXxi7g7QYWYfHWXk6nkWXAjdMRhbnTgC";
 
 export const கிளிமூக்கு_தரவுத்தளம்_வார்ப்புரு: bds.schémaSpécificationBd = {
-  motsClefs: [
-    மொழியாக்கம்_சிறப்பு_சொல்_அடையாளம்,
-    கிளிமூக்கு_சிறப்பு_சொல்_அடையாளம்,
-  ],
   licence: "ODbl-1_0",
   tableaux: [
     {
       clef: மொழிபெயர்ப்பு_அட்டவணை_சாபி,
       cols: [
         {
-          idVariable: சாபி_மாறி_அடையாளம்,
           idColonne: சாபி_நெடுவரிசை_அடையாளம்,
-          index: true,
         },
         {
-          idVariable: மூல்_மொழி_மாறி_அடையாளம்,
           idColonne: மூல்_மொழி_நெடுவரிசை_அடையாளம்,
         },
         {
-          idVariable: இலக்கு_மொழி_மாறி_அடையாளம்,
           idColonne: இலக்கு_மொழி_நெடுவரிசை_அடையாளம்,
-          index: true,
         },
         {
-          idVariable: மூல்_உரை_மாறி_அடையாளம்,
           idColonne: மூல்_உரை_நெடுவரிசை_அடையாளம்,
         },
         {
-          idVariable: மொழிபெயர்ப்பு_மாறி_அடையாளம்,
           idColonne: மொழிபெயர்ப்பு_நெடுவரிசை_அடையாளம்,
         },
         {
-          idVariable: தேதி_மாறி_அடையாளம்,
           idColonne: தேதி_நெடுவரிசை_அடையாளம்,
         },
       ],
